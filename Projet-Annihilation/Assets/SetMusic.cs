b63500bacb1725681adfa0e93ee6305128602fe8
byoman <yann.bonnet@epita.fr>
2015-06-18 19:33:14 +0200

    Menu Options

---
+++ Projet-Annihilation/Assets/SetMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void setVolume () {
+		float hSliderValue = 0f;
+		onGUI (hSliderValue);
+		AudioListener.volume = hSliderValue;
+	}
+	void onGUI(float hSliderValue)
+	{	
+
+		Rect rect = new Rect (-30, 130, -135, 115);
+		hSliderValue = GUI.HorizontalSlider (rect, hSliderValue, 0f, 10f);
+		 hSliderValue= hSliderValue/10f;
+	}
+}
---
+++ Projet-Annihilation/Assets/manage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void changeToScene (int scene) {
+		Application.LoadLevel (scene);
+	}
+
+}
