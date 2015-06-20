b644f294eafdd3ea3f1c75c73978b60184ef7ccd
byoman <yann.bonnet@epita.fr>
2015-06-20 19:16:45 +0200

    menu options
    debut menu ok

---
+++ Projet-Annihilation/Assets/gomain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gomain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void gotomain (int scene) {
+		Application.LoadLevel (scene);
+	}
+}
---
+++ Projet-Annihilation/Assets/setresolution.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setresolution : MonoBehaviour {
+	private int larg;
+	private int haut;
+	private bool windowed;
+
+	public void switchres(int lar, int hau)
+	{	larg = lar;
+		haut = hau;
+		Screen.SetResolution (lar, hau, !windowed);
+	}
+	void onGUI()
+	{	
+		Rect rect = new Rect (-80, 80, -60, -40);
+		windowed = GUI.Toggle (rect, windowed, "windowed");
+
+	}
+	public void window()
+	{
+		onGUI ();
+		switchres (larg, haut);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
